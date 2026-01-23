374d5c2faac9cd2563439b790851db269f6aaa80
SukaziN <nkosiyazisukazie@gmail.com>
2026-01-23 06:50:37 +0200

    added selectors on buttons and functionality for the computer's move

--- script.js
+++ script.js
@@ -3,4 +3,13 @@ let computerMoves = Math.floor(Math.random() * 3);
 console.log(computerMoves);
 console.log(options[computerMoves]);
 
-function play() {}
+let rockButton = document.getElementById("rock");
+let paperButton = document.getElementById("paper");
+let scissorsButton = document.getElementById("scissors");
+function play() {
+  let move = options[computerMoves];
+  let message = `Computer chose ${move}`;
+  //if (move == userMove) {
+  //console.log("Congratulations! You win!");
+  // }
+}
