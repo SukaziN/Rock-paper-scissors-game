0789b1e8995d905f267e68b2bcceb25dacb73994
SukaziN <nkosiyazisukazie@gmail.com>
2026-01-23 15:04:07 +0200

    added logic for functionality

--- script.js
+++ script.js
@@ -4,11 +4,34 @@ console.log(computerMoves);
 console.log(options[computerMoves]);
 
 let rockButton = document.getElementById("rock");
+//rockButton.addEventListener("click", console.log(`your move: ${option[0]}`)); attempted to make a button click output a message(ERROR)
+rockButton = options[0];
+console.log(rockButton);
 let paperButton = document.getElementById("paper");
+paperButton = options[1];
+console.log(paperButton);
 let scissorsButton = document.getElementById("scissors");
+scissorsButton = options[2];
+console.log(scissorsButton);
 function play() {
   let move = options[computerMoves];
   let message = `Computer chose ${move}`;
+  switch (move) {
+    case move === rockButton:
+    case move === paperButton:
+    case move === scissorsButton:
+      console.log(
+        `You and the computer guessed the same move: ${move}. Try again!`,
+      );
+      break;
+    case move === "Rock" && move === scissorsButton:
+    case move === "Scissors" && move === paperButton:
+    case move === "Paper" && move === rockButton:
+      console.log(message.concat(" You lose!"));
+      break;
+    default:
+      console.log(message.concat(" You Win!"));
+  }
   //if (move == userMove) {
   //console.log("Congratulations! You win!");
   // }
