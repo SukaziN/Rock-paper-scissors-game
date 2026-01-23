fc6edc04530d5a5c1fcb9d5070fabd4f66693cf1
SukaziN <nkosiyazisukazie@gmail.com>
2026-01-23 06:19:52 +0200

    declared variables for game options and computer's moves

---
+++ script.js
@@ -0,0 +1,6 @@
+let options = ["Rock", "Paper", "Scissors"];
+let computerMoves = Math.floor(Math.random() * 3);
+console.log(computerMoves);
+console.log(options[computerMoves]);
+
+function play() {}
