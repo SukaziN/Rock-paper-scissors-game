0d2d6722bfff9a1d91ef59e1653d4fbc7ae57e82
SukaziN <nkosiyazisukazie@gmail.com>
2026-01-24 06:57:05 +0200

    analyzed else if statement, left notes and added else statement

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ console.log(paperButton.value);
 let scissorsButton = document.getElementById("scissors");
 //scissorsButton = options[2];
 console.log(scissorsButton.value);
+let move = options[computerMoves];
 function play() {
-  let move = options[computerMoves];
   let message = `Computer chose ${move}`;
   if (
     move == rockButton.value || //the conditional works but cannot differentiate between the buttons and is therefore assigning the same value("rock") to all of them
@@ -25,15 +25,15 @@ function play() {
       `You and the computer guessed the same move: ${move}. Try again!`,
     );
   } else if (
-    (move == "Rock" && move === scissorsButton.value) || //the logic after && needs to relate to the user's move and this line refers to "move" which was declared as the computer's move
-    (move == "Scissors" && move === paperButton.value) || //store the user's move in a different variable and use it after &&
-    (move == "Paper" && move === rockButton.value)
+    (move == "Rock" && move == scissorsButton.value) || //the logic after && needs to relate to the user's move and this line refers to "move" which was declared as the computer's move
+    (move == "Scissors" && move == paperButton.value) || //store the user's move in a different variable and use it after &&
+    (move == "Paper" && move == rockButton.value) //add an event listener such that the value of the button is fetched when the specific button is clicked (this should replace the "move" after &&, name it userMove in the declaration)
   ) {
     console.log(message.concat(" You lose!"));
   }
 
-  //else:
-  //console.log(message.concat(" You Win!")); so far this is the only logic going through when the function is invoked (meaning the logic above either isn't linking to any specific buttons or it's invalid or both)
+  else{
+  console.log(message.concat(" You Win!"));} 
 }
 //if (move == userMove) {
 //console.log("Congratulations! You win!");
