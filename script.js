d229dfb7dfeb7d6544d6b881aee95ae6a5922421
SukaziN <nkosiyazisukazie@gmail.com>
2026-01-24 06:46:20 +0200

    changed the switch statement into a regular if else statement for less strict comparison

--- script.js
+++ script.js
@@ -16,26 +16,25 @@ console.log(scissorsButton.value);
 function play() {
   let move = options[computerMoves];
   let message = `Computer chose ${move}`;
-  switch (
-    move //switch statements are strict equality checkers and its possible that the logic below could be better understood with a regular if else statement
+  if (
+    move == rockButton.value || //the conditional works but cannot differentiate between the buttons and is therefore assigning the same value("rock") to all of them
+    move == paperButton.value ||
+    move == scissorsButton.value
   ) {
-    case move == rockButton.value:
-    case move == paperButton.value:
-    case move == scissorsButton.value:
-      console.log(
-        `You and the computer guessed the same move: ${move}. Try again!`,
-      );
-      break;
-    case move == "Rock" && move === scissorsButton.value: //the logic after && needs to relate to the user's move and this line refers to "move" which was declared as the computer's move
-    case move == "Scissors" && move === paperButton.value: //store the user's move in a different variable and use it after &&
-    case move == "Paper" && move === rockButton.value:
-      console.log(message.concat(" You lose!"));
-      break;
-    //default:
-    //console.log(message.concat(" You Win!")); so far this is the only logic going through when the function is invoked (meaning the logic above either isn't linking to any specific buttons or it's invalid or both)
-    //break;
+    console.log(
+      `You and the computer guessed the same move: ${move}. Try again!`,
+    );
+  } else if (
+    (move == "Rock" && move === scissorsButton.value) || //the logic after && needs to relate to the user's move and this line refers to "move" which was declared as the computer's move
+    (move == "Scissors" && move === paperButton.value) || //store the user's move in a different variable and use it after &&
+    (move == "Paper" && move === rockButton.value)
+  ) {
+    console.log(message.concat(" You lose!"));
   }
-  //if (move == userMove) {
-  //console.log("Congratulations! You win!");
-  // }
+
+  //else:
+  //console.log(message.concat(" You Win!")); so far this is the only logic going through when the function is invoked (meaning the logic above either isn't linking to any specific buttons or it's invalid or both)
 }
+//if (move == userMove) {
+//console.log("Congratulations! You win!");
+// }
