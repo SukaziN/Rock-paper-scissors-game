10a1dfdda207224b58f2a0816599aec0ab1098e3
SukaziN <nkosiyazisukazie@gmail.com>
2026-02-04 07:58:22 +0200

    reversed redundant changes and modified message displayed when it's a tie

--- script.js
+++ script.js
@@ -1,27 +1,20 @@
-/*let options = ["Rock", "Paper", "Scissors"];
+let options = ["Rock", "Paper", "Scissors"];
 let computerMoves = Math.floor(Math.random() * 3);
-let move = options[computerMoves]; */
+let move = options[computerMoves];
 
-//let rockButton = document.getElementById("rock");
+let rockButton = document.getElementById("rock");
 
-//let paperButton = document.getElementById("paper");
+let paperButton = document.getElementById("paper");
 
-//let scissorsButton = document.getElementById("scissors");
+let scissorsButton = document.getElementById("scissors");
 
-/*let userMoveRock = rockButton.value;
+let userMoveRock = rockButton.value;
 let userMovePaper = paperButton.value;
-let userMoveScissors = scissorsButton.value; */
-//let message = `Computer chose ${move}`;
-//let outcome = document.getElementById("outcome");
+let userMoveScissors = scissorsButton.value;
+let message = `Computer chose ${move}`;
+let outcome = document.getElementById("outcome");
 
 function playRock() {
-  let options = ["Rock", "Paper", "Scissors"];
-  let computerMoves = Math.floor(Math.random() * 3);
-  let move = options[computerMoves];
-  let rockButton = document.getElementById("rock");
-  let userMoveRock = rockButton.value;
-  let message = `Computer chose ${move}`;
-  let outcome = document.getElementById("outcome");
   if (move == userMoveRock) {
     outcome.innerHTML = `You and the computer guessed the same move: ${move}. It's a tie!`;
   } else if (move == "Paper" && userMoveRock) {
@@ -32,15 +25,8 @@ function playRock() {
 }
 
 function playPaper() {
-  let options = ["Rock", "Paper", "Scissors"];
-  let computerMoves = Math.floor(Math.random() * 3);
-  let move = options[computerMoves];
-  let paperButton = document.getElementById("paper");
-  let userMovePaper = paperButton.value;
-  let message = `Computer chose ${move}`;
-  let outcome = document.getElementById("outcome");
   if (move == userMovePaper) {
-    outcome.innerHTML = `You and the computer guessed the same move: ${move}. Try again!`;
+    outcome.innerHTML = `You and the computer guessed the same move: ${move}. It's a tie!`;
   } else if (move == "Scissors" && userMovePaper) {
     outcome.innerHTML = message.concat(" You lose!😞");
   } else {
@@ -48,15 +34,8 @@ function playPaper() {
   }
 }
 function playScissors() {
-  let options = ["Rock", "Paper", "Scissors"];
-  let computerMoves = Math.floor(Math.random() * 3);
-  let move = options[computerMoves];
-  let scissorsButton = document.getElementById("scissors");
-  let userMoveScissors = scissorsButton.value;
-  let message = `Computer chose ${move}`;
-  let outcome = document.getElementById("outcome");
   if (move == userMoveScissors) {
-    outcome.innerHTML = `You and the computer guessed the same move: ${move}. Try again!`;
+    outcome.innerHTML = `You and the computer guessed the same move: ${move}. It's a tie!`;
   } else if (move == "Rock" && userMoveScissors) {
     outcome.innerHTML = message.concat(" You lose!😞");
   } else {
